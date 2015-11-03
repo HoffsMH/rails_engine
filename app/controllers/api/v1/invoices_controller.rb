4c6b127f2b3ad75572136c81a846255b753e4408
HoffsMH <hecker.matthew@gmail.com>
2015-11-02 23:16:10 -0700

    added invoice_item assoc on invoice controller

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,2 +1,23 @@
-class Api::V1::InvoicesController < ApplicationController
+class Api::V1::InvoicesController < Api::V1::BaseApiController
+  def object_type
+    Invoice
+  end
+
+  def items
+    binding.pry
+    respond_with Item.where(invoice_params)
+  end
+
+  def invoice_items
+    respond_with InvoiceItem.where(invoice_params)
+  end
+
+  def invoices
+    respond_with Invoice.where(invoice_params)
+  end
+
+  private
+  def invoice_params
+    params.permit(:invoice_id)
+  end
 end
