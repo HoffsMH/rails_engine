16bbfcc2e386fa9cb033a23e10975fc4247c839d
HoffsMH <hecker.matthew@gmail.com>
2015-11-02 22:31:55 -0700

    completely migrated basic functions to base controller

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -3,4 +3,26 @@ class Api::V1::BaseApiController < ApplicationController
     def index
       respond_with object_type.all
     end
+
+    def show
+      respond_with object_type.find_by(id: show_params)
+    end
+
+    def find
+      respond_with  object_type.find_by(find_params)
+    end
+
+    def find_all
+      respond_with  object_type.where(find_params)
+    end
+
+    private
+    def find_params
+      object_type_params = object_type.column_names.map {|name| name.to_sym}
+      params.permit(*object_type_params)
+    end
+
+    def show_params
+      params.require(:id)
+    end
 end
--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,27 +1,5 @@
 class Api::V1::MerchantsController < Api::V1::BaseApiController
-  def index
-    respond_with object_type.all
-  end
-
-  def show
-    respond_with object_type.find_by(id: show_params)
-  end
-
-  def find
-    respond_with  object_type.find_by(find_params)
-  end
-
-  private
   def object_type
     Merchant
-  end
-
-  def show_params
-    params.require(:id)
-  end
-
-  def find_params
-    merchant_params = object_type.column_names.map {|name| name.to_sym}
-    params.permit(*merchant_params)
   end
 end
