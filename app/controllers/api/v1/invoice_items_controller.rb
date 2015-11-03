13982d8e911d5af7d050dc82c558bfb3cb3e52d5
HoffsMH <hecker.matthew@gmail.com>
2015-11-03 00:09:47 -0700

    added item action to invoice_items controller

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -7,6 +7,10 @@ class Api::V1::InvoiceItemsController < Api::V1::BaseApiController
     respond_with InvoiceItem.find_by(id: invoice_items_params[:invoice_item_id]).invoice
   end
 
+  def item
+    respond_with InvoiceItem.find_by(id: invoice_items_params[:invoice_item_id]).item
+  end
+
   private
   def invoice_items_params
     params.permit(:invoice_item_id)
