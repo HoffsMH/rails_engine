4f55547236083975ab6b90900f476580d97d8b7a
HoffsMH <hecker.matthew@gmail.com>
2015-11-02 19:35:20 -0700

    added name to Merchants
    other than timestamps and id all merchant has is name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103022247) do
+ActiveRecord::Schema.define(version: 20151103023308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151103022247) do
   create_table "merchants", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
 end
