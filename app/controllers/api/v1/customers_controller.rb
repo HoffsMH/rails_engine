8e14756506765553291d7f04861b5097c74e6b6d
HoffsMH <hecker.matthew@gmail.com>
2015-11-03 00:35:05 -0700

    added template actions to customer controller

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,2 +1,10 @@
-class Api::V1::CustomersController < ApplicationController
+class Api::V1::CustomersController < Api::V1::BaseApiController
+  def object_type
+    Transaction
+  end
+
+  private
+  def transaction_params
+    params.permit(:transaction_id)
+  end
 end
