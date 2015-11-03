15fa3a9e37891d89cb3c33c191a3c39da8c90066
HoffsMH <hecker.matthew@gmail.com>
2015-11-02 21:25:43 -0700

    finished transaction model and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,36 +10,40 @@ require 'csv'
 
 def load_csv(file_path, model_type)
   args = {:headers => true,
-          :header_converters => :symbol,
-          :converters => :all}
-    count = 0
-    model_type.transaction do
-      CSV.foreach(file_path, args) do |row|
-        record = model_type.new()
-        row.headers.each_with_index do |header, index|
-          if header != :id
-            record.update(header => row[index])
-          end
+    :header_converters => :symbol,
+    :converters => :all}
+  count = 0
+  model_type.transaction do
+    CSV.foreach(file_path, args) do |row|
+      record = model_type.new()
+      row.headers.each_with_index do |header, index|
+        if header != :id
+          record.update(header => row[index])
         end
-        count += 1
-        print '.' if (count % 10) == 0
       end
+      count += 1
+      print '.' if (count % 10) == 0
     end
-    puts "Successfully loaded #{count} #{model_type.to_s}s into database!"
   end
+  puts "Successfully loaded #{count} #{model_type.to_s}s into database!"
+end
 
-  merchants_path  = "db/data/merchants.csv"
-  model_type = Merchant
-  load_csv(merchants_path, model_type)
+merchants_path  = "db/data/merchants.csv"
+model_type = Merchant
+load_csv(merchants_path, model_type)
 
-  customers_path  = "db/data/customers.csv"
-  model_type = Customer
-  load_csv(customers_path, model_type)
+customers_path  = "db/data/customers.csv"
+model_type = Customer
+load_csv(customers_path, model_type)
 
-  items_path  = "db/data/items.csv"
-  model_type = Item
-  load_csv(items_path, model_type)
+items_path  = "db/data/items.csv"
+model_type = Item
+load_csv(items_path, model_type)
 
-  invoices_path  = "db/data/invoices.csv"
-  model_type = Invoice
-  load_csv(invoices_path, model_type)
+invoices_path  = "db/data/invoices.csv"
+model_type = Invoice
+load_csv(invoices_path, model_type)
+
+transactions_path  = "db/data/transactions.csv"
+model_type = Transaction
+load_csv(transactions_path, model_type)
