38201d05831bdfe2f51672c8f9c7118ef8aeda50
HoffsMH <hecker.matthew@gmail.com>
2015-11-03 00:11:55 -0700

    added template  actions to items

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,18 @@
-class Api::V1::ItemsController < ApplicationController
+class Api::V1::ItemsController < Api::V1::BaseApiController
+  def object_type
+    InvoiceItem
+  end
+
+  def invoice
+    respond_with InvoiceItem.find_by(id: invoice_items_params[:invoice_item_id]).invoice
+  end
+
+  def item
+    respond_with InvoiceItem.find_by(id: invoice_items_params[:invoice_item_id]).item
+  end
+
+  private
+  def invoice_items_params
+    params.permit(:invoice_item_id)
+  end
 end
