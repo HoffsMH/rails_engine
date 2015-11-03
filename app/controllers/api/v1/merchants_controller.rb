dce5bd259b4ee29e54b783af124df4ff19de9742
HoffsMH <hecker.matthew@gmail.com>
2015-11-02 22:52:30 -0700

    added items association for merchant items

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,4 +2,13 @@ class Api::V1::MerchantsController < Api::V1::BaseApiController
   def object_type
     Merchant
   end
+
+  def items
+    respond_with Item.where(merchant_params)
+  end
+
+  private
+  def merchant_params
+    params.permit(:merchant_id)
+  end
 end
