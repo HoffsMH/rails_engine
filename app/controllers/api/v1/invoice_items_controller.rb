10831480c0a8c9a39eabd489cce8936c9e15a162
HoffsMH <hecker.matthew@gmail.com>
2015-11-02 23:58:48 -0700

    fixed mistake in invoice item controller

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -6,6 +6,6 @@ class Api::V1::InvoiceItemsController < Api::V1::BaseApiController
 
   private
   def invoice_items_params
-    params.permit(:invoice_id)
+    params.permit(:invoice_item_id)
   end
 end
