bafb0388eebd2b603aa68ceccf8ee40a6e6bef3e
HoffsMH <hecker.matthew@gmail.com>
2015-11-02 21:51:57 -0700

    added find method to merchants controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -7,7 +7,9 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     respond_with Merchant.find_by(id: merchant_params)
   end
-  
+
+  def find
+  end
   private
   def merchant_params
     params.require(:id)
