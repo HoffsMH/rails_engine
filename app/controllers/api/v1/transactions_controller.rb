89cb22c1c997f0f2b502ed418e25d92aa76d376f
HoffsMH <hecker.matthew@gmail.com>
2015-11-03 00:32:56 -0700

    added invoice action to transaction  controller

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -3,6 +3,10 @@ class Api::V1::TransactionsController < Api::V1::BaseApiController
     Transaction
   end
 
+  def invoice
+    respond_with Transaction.find_by(id: transaction_params[:transaction_id]).invoice
+  end
+
 
   private
   def transaction_params
