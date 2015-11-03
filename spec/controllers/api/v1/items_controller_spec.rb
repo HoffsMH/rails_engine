f67b6fdd49cdff01df21b8dea2b285d2d6034e29
HoffsMH <hecker.matthew@gmail.com>
2015-11-03 09:57:43 -0700

    added test for show
    also added test for index
    making sure we are displaying correct amount of records

--- spec/controllers/api/v1/items_controller_spec.rb
+++ spec/controllers/api/v1/items_controller_spec.rb
@@ -1,11 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::ItemsController, type: :controller do
-  it "gets an index of all items" do
-    # get "api/v1/items"
-    get :index, format: :json
+  context "#index action" do
+    it "gets an index" do
+      get :index, format: :json
 
-    expect(response.status).to eq(200)
+      expect(response.status).to eq(200)
+    end
+
+    it "gets an index of all items" do
+      merchant = Merchant.create(name: "some_merchant_name")
+      Item.create(name: "some_item_name", merchant_id: merchant.id)
+      get :index, format: :json
+
+      json_response = JSON.parse(response.body)
+      number_of_items = Item.all.count
+
+      expect(json_response.count).to eq(number_of_items)
+    end
+  end
+
+  context "#show action" do
+    it "gets an item" do
+      get :show, format: :json, id: 1
+
+      expect(response.status).to eq(200)
+    end
+
+    it "displays the item" do
+      merchant = Merchant.create(name: "some_merchant_name")
+      item = Item.create(name: "some_item_name", merchant_id: merchant.id)
+      get :show, format: :json, id: item.id
+
+      json_response = JSON.parse(response.body)
+
+      expect(json_response.count).to eq(Item.column_names.count)
+    end
+
+    it "shows information for the item" do
+      merchant = Merchant.create(name: "some_merchant_name")
+      item = Item.create(name: "some_item_name", merchant_id: merchant.id)
+      get :show, format: :json, id: item.id
+
+      json_response = JSON.parse(response.body)
+
+      expect(json_response.first).to contain("some_item_name")
+    end
   end
 
 end
