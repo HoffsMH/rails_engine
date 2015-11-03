9d8432a46e6a818802ac6d41d36b1d564ecd7445
HoffsMH <hecker.matthew@gmail.com>
2015-11-02 23:55:39 -0700

    template for invoice items controller in place

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -1,30 +1,11 @@
-class Api::V1::InvoiceItemsController < ApplicationController
+class Api::V1::InvoiceItemsController < Api::V1::BaseApiController
   def object_type
-    Invoice
+    InvoiceItem
   end
 
-  def items
-    respond_with Item.joins(:invoice_items).where(invoice_items: invoice_params)
-  end
-
-  def invoice_items
-    respond_with InvoiceItem.where(invoice_params)
-  end
-
-  def transactions
-    respond_with Transaction.where(invoice_params)
-  end
-
-  def customer
-    respond_with Invoice.find_by(id: invoice_params[:invoice_id]).customer
-  end
-
-  def merchant
-    respond_with Invoice.find_by(id: invoice_params[:invoice_id]).merchant
-  end
 
   private
-  def invoice_params
+  def invoice_items_params
     params.permit(:invoice_id)
   end
 end
