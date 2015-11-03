b6e25abf477b1d66daa43f4ef7da7aedf2668ae1
HoffsMH <hecker.matthew@gmail.com>
2015-11-02 22:21:34 -0700

    converting for generic use
    this controller still currently works but getting ready to apply it genericly

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,18 +1,29 @@
 class Api::V1::MerchantsController < ApplicationController
+
   respond_to :json
   def index
-    respond_with Merchant.all
+    respond_with object_type.all
   end
 
   def show
-    respond_with Merchant.find_by(id: merchant_params)
+    respond_with object_type.find_by(id: show_params)
   end
 
   def find
-
+    respond_with  object_type.find_by(find_params)
   end
+
   private
-  def merchant_params
+  def object_type
+    Merchant
+  end
+
+  def show_params
     params.require(:id)
+  end
+
+  def find_params
+    merchant_params = object_type.column_names.map {|name| name.to_sym}
+    params.permit(*merchant_params)
   end
 end
