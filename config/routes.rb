921187b58336a370856430cd9f8e58398271b567
HoffsMH <hecker.matthew@gmail.com>
2015-11-02 22:37:12 -0700

    added random route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/merchants/find', to: 'merchants#find', defaults: {format: 'json'}
       get '/merchants/find_all', to: 'merchants#find_all', defaults: {format: 'json'}
+      get '/merchants/random', to: 'merchants#random', defaults: {format: 'json'}
       resources :merchants, except: [:new, :edit, :update, :create], defaults: {format: 'json'} do
         get '/items', to: 'merchants/items#index'
       end
