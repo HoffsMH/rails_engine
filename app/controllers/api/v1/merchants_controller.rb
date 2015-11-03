686e53974683d9df766505f7468e807142d1139f
HoffsMH <hecker.matthew@gmail.com>
2015-11-02 21:51:30 -0700

    added index and show for merchant

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::MerchantsController < ApplicationController
+  respond_to :json
   def index
     respond_with Merchant.all
+  end
+
+  def show
+    respond_with Merchant.find_by(id: merchant_params)
+  end
+  
+  private
+  def merchant_params
+    params.require(:id)
   end
 end
