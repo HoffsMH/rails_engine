b7d0e5c602730380b800a5fe50ba01bfac902e05
HoffsMH <hecker.matthew@gmail.com>
2015-11-03 00:14:17 -0700

    got rid of filler methods in items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,14 +3,6 @@ class Api::V1::ItemsController < Api::V1::BaseApiController
     Item
   end
 
-  def invoice
-    respond_with InvoiceItem.find_by(id: invoice_items_params[:invoice_item_id]).invoice
-  end
-
-  def item
-    respond_with InvoiceItem.find_by(id: invoice_items_params[:invoice_item_id]).item
-  end
-
   private
   def item_params
     params.permit(:item_id)
