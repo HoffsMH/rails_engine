aae786cae1993b6b2a175dd6dea05db85ffa9190
HoffsMH <hecker.matthew@gmail.com>
2015-11-03 19:36:39 -0700

    fixing factories
    put braces around faker gem inserts so that its random everytime upon creation

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :invoice_item do
     item
     invoice
-    quantity Random.rand(1..10)
-    unit_price Random.rand(1000..100000)
+    quantity { Random.rand(1..10) }
+    unit_price { Random.rand(1000..100000) }
   end
 end
--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :transaction do
     invoice
-    credit_card_number Faker::Business.credit_card_number
-    credit_card_expiration_date Faker::Business.credit_card_expiry_date
+    credit_card_number { Faker::Business.credit_card_number }
+    credit_card_expiration_date { Faker::Business.credit_card_expiry_date }
     result ["success", "failed"].sample
   end
 end
