78ea45ff4e2582d9349e0b4a21e9fa4d79ea2ef3
HoffsMH <hecker.matthew@gmail.com>
2015-11-03 21:51:59 -0700

    added test for invoice controller
    we are now testing the transactions action

--- spec/controllers/api/v1/invoices_controller_spec.rb
+++ spec/controllers/api/v1/invoices_controller_spec.rb
@@ -76,4 +76,47 @@ RSpec.describe Api::V1::InvoicesController, type: :controller do
     end
   end
 
+  describe "#transactions" do
+    context "when given valid params" do
+      it "it returns the invoice's transactions" do
+        invoice = create(:invoice)
+
+        item1 = create(:item)
+        item2 = create(:item)
+
+        transaction1 = create(:transaction, invoice_id: invoice.id)
+        transaction2 = create(:transaction, invoice_id: invoice.id)
+
+        invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice.id)
+        invoice_item2 = create(:invoice_item, item_id: item2.id, invoice_id: invoice.id)
+
+
+        get :transactions, format: :json, invoice_id: invoice.id
+
+        expect(response.body).to include("\"invoice_id\":#{invoice.id}")
+        expect(json.count).to eq(2)
+      end
+    end
+
+    context "when given invalid params" do
+      it "it returns 404 status and an error message" do
+        invoice = create(:invoice)
+
+        item1 = create(:item)
+        item2 = create(:item)
+
+        transaction1 = create(:transaction, invoice_id: invoice.id)
+        transaction2 = create(:transaction, invoice_id: invoice.id)
+
+        invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice.id)
+        invoice_item2 = create(:invoice_item, item_id: item2.id, invoice_id: invoice.id)
+
+        get :transactions, format: :json, invoice_id: 9929992
+
+        expect(response.body).to include("not found")
+        expect(response.status).to eq(404)
+      end
+    end
+  end
+
 end
