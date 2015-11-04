00649563b0c226894b92489ddf8a52d9f455e6e6
HoffsMH <hecker.matthew@gmail.com>
2015-11-03 18:24:45 -0700

    cleaning up controller test of comments

--- spec/controllers/api/v1/customers_controller_spec.rb
+++ spec/controllers/api/v1/customers_controller_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
+require 'support/basic_endpoints'
 
 RSpec.describe Api::V1::CustomersController, type: :controller do
-
+  test_basic_endpoints(Customer)
 end
--- spec/controllers/api/v1/items_controller_spec.rb
+++ spec/controllers/api/v1/items_controller_spec.rb
@@ -3,55 +3,16 @@ require 'support/basic_endpoints'
 
 RSpec.describe Api::V1::ItemsController, type: :controller do
   test_basic_endpoints(Item)
+  describe "#invoice items" do
+    context "when given valid params" do
+      it "it returns the item's invoice items" do
+        item = create(:item)
+        create(:invoice_item, item_id: item.id)
+        invoice_item = create(:invoice_item, item_id: item.id)
 
-  # context "#index action" do
-  #   it "gets an index" do
-  #     get :index, format: :json
-  #
-  #     expect(response.status).to eq(200)
-  #   end
-  #
-  #   it "gets an index of all items" do
-  #     binding.pry
-  #     merchant = Merchant.create(name: "some_merchant_name")
-  #
-  #     Item.create(name: "some_item_name", merchant_id: merchant.id)
-  #     get :index, format: :json
-  #
-  #     json_response = JSON.parse(response.body)
-  #     number_of_items = Item.all.count
-  #
-  #     expect(json_response.count).to eq(number_of_items)
-  #   end
-  # end
-  #
-  # context "#show action" do
-  #   it "gets an item" do
-  #     get :show, format: :json, id: 1
-  #
-  #     expect(response.status).to eq(200)
-  #   end
-  #
-  #   it "displays the item" do
-  #     merchant = Merchant.create(name: "some_merchant_name")
-  #     item = Item.create(name: "some_item_name", merchant_id: merchant.id)
-  #     get :show, format: :json, id: item.id
-  #
-  #     json_response = JSON.parse(response.body)
-  #
-  #     expect(json_response.count).to eq(Item.column_names.count)
-  #   end
-  #
-  #   it "shows information for the item" do
-  #     merchant = Merchant.create(name: "some_merchant_name")
-  #     item = Item.create(name: "some_item_name", merchant_id: merchant.id, description: "blah blah")
-  #     get :show, format: :json, id: item.id
-  #
-  #     json_response = JSON.parse(response.body)
-  #
-  #     expect(json_response["name"]).to eq(item.name)
-  #     expect(json_response["description"]).to eq(item.description)
-  #   end
-  # end
-
+        get :invoice_items, format: :json, item_id: item.id
+        binding.pry
+      end
+    end
+  end
 end
