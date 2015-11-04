cd67013ddd51880d86704af472c8710e79819f93
HoffsMH <hecker.matthew@gmail.com>
2015-11-03 20:36:34 -0700

    added tests for invoice_items controller
    now testing the invoice action

--- spec/controllers/api/v1/invoice_items_controller_spec.rb
+++ spec/controllers/api/v1/invoice_items_controller_spec.rb
@@ -2,32 +2,30 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::InvoiceItemsController, type: :controller do
   test_basic_endpoints(InvoiceItem)
-  describe "#items" do
+  describe "#invoice" do
     context "when given valid params" do
-      it "it returns the merchant's items" do
+      it "it returns invoice_item's invoice" do
         merchant = create(:merchant)
-        create(:item, merchant_id: merchant.id)
-        create(:item)
-        create(:item, merchant_id: merchant.id)
-        create(:item)
+        invoice = create(:invoice, merchant_id: merchant.id)
+        create(:invoice_item, invoice_id: invoice.id)
+        create(:invoice_item, invoice_id: invoice.id)
+        invoice_item = create(:invoice_item, invoice_id: invoice.id)
 
-
-        get :items, format: :json, merchant_id: merchant.id
+        get :invoice, format: :json, invoice_item_id: invoice_item.id
 
         expect(response.body).to include("\"merchant_id\":#{merchant.id}")
-        expect(json.count).to eq(2)
       end
     end
 
     context "when given invalid params" do
       it "it returns 404 status and an error message" do
         merchant = create(:merchant)
-        create(:item, merchant_id: merchant.id)
-        create(:item)
-        create(:item, merchant_id: merchant.id)
-        create(:item)
+        invoice = create(:invoice, merchant_id: merchant.id)
+        create(:invoice_item, invoice_id: invoice.id)
+        create(:invoice_item, invoice_id: invoice.id)
+        invoice_item = create(:invoice_item, invoice_id: invoice.id)
 
-        get :items, format: :json, merchant_id: 9929992
+        get :invoice, format: :json, invoice_item_id: 9929992
 
         expect(response.body).to include("not found")
         expect(response.status).to eq(404)
