32eb56c65c39c408eb9efda904248d16af31b289
HoffsMH <hecker.matthew@gmail.com>
2015-11-03 19:46:02 -0700

    customer controller test
    added scaffolding soon to be changed to the appropriate test

--- spec/controllers/api/v1/customers_controller_spec.rb
+++ spec/controllers/api/v1/customers_controller_spec.rb
@@ -3,4 +3,31 @@ require 'support/basic_endpoints'
 
 RSpec.describe Api::V1::CustomersController, type: :controller do
   test_basic_endpoints(Customer)
+  describe "#invoice items" do
+    context "when given valid params" do
+      it "it returns the item's invoice items" do
+        item = create(:item)
+        create(:invoice_item, item_id: item.id)
+        invoice_item = create(:invoice_item, item_id: item.id)
+
+        get :invoice_items, format: :json, item_id: item.id
+
+        expect(response.body).to include("\"item_id\":#{item.id}")
+        expect(json.count).to eq(2)
+      end
+    end
+
+    context "when given invalid params" do
+      it "it returns 404 status and an error message" do
+        item = create(:item)
+        create(:invoice_item, item_id: item.id)
+        invoice_item = create(:invoice_item, item_id: item.id)
+
+        get :invoice_items, format: :json, item_id: 9929992
+
+        expect(response.body).to include("not found")
+        expect(response.status).to eq(404)
+      end
+    end
+  end
 end
