8736634b11c273b6ebc5101abfb8071998c92fde
HoffsMH <hecker.matthew@gmail.com>
2015-11-03 21:17:36 -0700

    testing transactions controller
    we are now testing the invoice action

--- spec/controllers/api/v1/transactions_controller_spec.rb
+++ spec/controllers/api/v1/transactions_controller_spec.rb
@@ -1,33 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::TransactionsController, type: :controller do
-  test_basic_endpoints(Merchant)
-  describe "#items" do
+  test_basic_endpoints(Transaction)
+  describe "#invoice" do
     context "when given valid params" do
-      it "it returns the merchant's items" do
-        merchant = create(:merchant)
-        create(:item, merchant_id: merchant.id)
-        create(:item)
-        create(:item, merchant_id: merchant.id)
-        create(:item)
+      it "it returns the transaction's invoice" do
+        invoice = create(:invoice)
+        transaction = create(:transaction, invoice_id: invoice.id)
 
+        get :invoice, format: :json, transaction_id: transaction.id
 
-        get :items, format: :json, merchant_id: merchant.id
-
-        expect(response.body).to include("\"merchant_id\":#{merchant.id}")
-        expect(json.count).to eq(2)
+        expect(response.body).to include("\"id\":#{invoice.id}")
       end
     end
 
     context "when given invalid params" do
       it "it returns 404 status and an error message" do
-        merchant = create(:merchant)
-        create(:item, merchant_id: merchant.id)
-        create(:item)
-        create(:item, merchant_id: merchant.id)
-        create(:item)
+        invoice = create(:invoice)
+        transaction = create(:transaction, invoice_id: invoice.id)
 
-        get :items, format: :json, merchant_id: 9929992
+        get :invoice, format: :json, transaction_id: 9929992
 
         expect(response.body).to include("not found")
         expect(response.status).to eq(404)
