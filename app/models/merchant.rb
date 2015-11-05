b020157f95da2e85dcb5c69a447708c104947288
HoffsMH <hecker.matthew@gmail.com>
2015-11-04 20:33:27 -0700

    ...

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -62,7 +62,6 @@ class Merchant < ActiveRecord::Base
 
   def favorite_customer
     # favorite = invoices.successful.joins(:transactions).group(:customer_id).count(:transactions)
-
     favorites = customers.select("customers.*, count(invoices.customer_id) as favorites").
               joins(invoices: :transactions).
               merge(Transaction.successful).
