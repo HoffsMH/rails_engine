d1d2aba4df7e5400b28e682db4beff811ba815e9
HoffsMH <hecker.matthew@gmail.com>
2015-11-04 23:48:16 -0700

    added test and support file for creating merchant connections to test merchant bi

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,9 +5,19 @@ RSpec.describe Merchant, type: :model do
     describe "#most_revenue" do
       it "returns the top x merchant ranked by total revenue" do
         100.times do
-          create(:merchant)
+          merchant = create(:merchant)
+          invoice = create(:invoice, merchant_id: merchant.id)
+          create(:transaction, invoice_id: invoice.id, result: "success")
+          create(:invoice_item, invoice_id: invoice.id)
+          create(:invoice_item, invoice_id: invoice.id)
+          create(:invoice_item, invoice_id: invoice.id)
+          create(:invoice_item, invoice_id: invoice.id)
         end
-        binding.pry
+        result = Merchant.most_revenue(2)
+
+        expect(result.count).to eq(2)
+        expect(result.count).to eq(2)
+
       end
     end
   end
