e99d3f123be801fce364eb28e398dd033d06c4b8
HoffsMH <hecker.matthew@gmail.com>
2015-11-04 23:05:55 -0700

    no including total as it breaks harness

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,5 +1,5 @@
 class InvoiceItemSerializer < ActiveModel::Serializer
-  attributes :id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at, :total
+  attributes :id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at
 
   def total
     object.unit_price * object.quantity
