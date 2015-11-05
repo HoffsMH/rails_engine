f7a91e0e53095b4ec481375dbd3bede40f8410e9
HoffsMH <hecker.matthew@gmail.com>
2015-11-04 23:03:04 -0700

    added serializer to invoice_items

---
+++ app/serializers/invoice_item_serializer.rb
@@ -0,0 +1,7 @@
+class InvoiceItemSerializer < ActiveModel::Serializer
+  attributes :id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at, :total
+
+  def total
+    object.unit_price * object.quantity
+  end
+end
